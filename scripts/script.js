3735add7465f6702271a56c4eeb2233c8c35654d
Florian Breit <77027788+flnbrt@users.noreply.github.com>
2023-05-31 14:04:24 +0200

    built main screen

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,68 @@
-// global variables
-const backgroundSize = [432, 768],
-    birdSize = [51, 36];
+/*
+   inspired by https://codepen.io/ju-az/pen/eYJQwLx/
+   licensed under MIT
+*/
 
-var canvas,
+// global variables
+const gameLoopInterval = 10,
+    background = {
+        startLeft: 0,
+        startTop: 0,
+        width: 432,
+        height: 768
+    },
+    bird = {
+        startLeft: 438,
+        startTop: 778,
+        width: 53,
+        height: 38
+    },
+    obstacle = {
+        topStartLeft: 168,
+        topStartTop: 969,
+        topWidth: 78,
+        topHeight: 480,
+        bottomStartLeft: 252,
+        bottomStartTop: 969,
+        bottomWidth: 78,
+        bottomHeight: 480
+    },
+    flappyBird = {
+        startLeft: 1053,
+        startTop: 273,
+        width: 267,
+        height: 72
+    },
+    playButton = {
+        startLeft: 1062,
+        startTop: 354,
+        width: 156,
+        height: 87
+    },
+    settingsButton = {
+    startLeft: 1062,
+    startTop: 461,
+    width: 156,
+    height: 87
+};
+
+var preloaded = false,
+    canvas,
     ctx,
-    world,
-    bird;
+    worldImage,
+    birdImage,
+    obstacleImage,
+    flappyBirdImage,
+    playButtonImage,
+    settingsButtonImage,
+    birdVersion = 0;
 
 // general game settings
 let gamePlaying = false,
-    speed = 6.0,
-    index = 0;
+    settingsOpened = false,
+    speed = 6.0 * (gameLoopInterval / 40),
+    animation = 0,
+    settingsPress = 0;
 
 
 // initialize
@@ -20,13 +72,6 @@ function init() {
     canvas = document.getElementById("gameCanvas");
     ctx = canvas.getContext("2d");
 
-    //background = new Image();
-    //background.src = "assets/FlappyBird_SpriteSheet.png";
-
-    //background = document.getElementById("background");
-
-    //document.getElementsByTagName("body");
-
     // preload all assets
     preloadAssets();
 }
@@ -34,16 +79,28 @@ function init() {
 // game loop
 function gameLoop() {
 
-    // increase index to display the animations
-    index++;
+    // increase animation index to display the animations
+    animation++;
 
-    // draw background
-    drawBackground();
+    // draw assets
+    drawAssets();
+
+    if (preloaded) {
+        // move position of buttons on click
+        /*
+        canvas.addEventListener("mousedown", function ()  { settingsPress = 2; console.log("settings pressed") });
+        canvas.addEventListener("mouseup", function ()  { settingsPress = 0; console.log("settings released") });
+        */
+
+        //document.getElementById("test").addEventListener("mouseup", () => console.log("test"));
+
+
+    }
 }
 
-/* 
-    preload assets
-    (c) Prof. Dr. Brändle
+/*
+   preload assets
+   (c) Prof. Dr. Brändle
 */
 function preloadAssets() {
 
@@ -64,22 +121,31 @@ function preloadAssets() {
         return img;
     }
 
-    // add world background
-    world = addImage("assets/FlappyBird_SpriteSheet_3x.png");
-    bird = addImage("assets/FlappyBird_SpriteSheet_3x.png");
-
     /*
-    world = addImage("https://i.ibb.co/Q9yv5Jk/flappy-bird-set.png");
-    bird = addImage("https://i.ibb.co/Q9yv5Jk/flappy-bird-set.png");
-    */
+        sprite sheet taken by https://www.spriters-resource.com/mobile/flappybird/sheet/59894/
+        (c) Superjustinbros
+     */
+
+    // add world background
+    worldImage = addImage("assets/FlappyBird_SpriteSheet_3x.png");
+    // add bird image
+    birdImage = addImage("assets/FlappyBird_SpriteSheet_3x.png");
+    // add obstacle image
+    obstacleImage = addImage("assets/FlappyBird_SpriteSheet_3x.png");
+    // add flappy bird image
+    flappyBirdImage = addImage("assets/FlappyBird_SpriteSheet_3x.png");
+    // add play button
+    playButtonImage = addImage("assets/FlappyBird_SpriteSheet_3x.png");
+    // add play button
+    settingsButtonImage = addImage("assets/FlappyBird_SpriteSheet_3x.png");
 
     // call gameLoop if every asset is loaded
     var checkResources = function () {
 
         if (preloadCount === 0) {
-            // every 40ms
-            console.log("gameLoop() called")
-            setInterval(gameLoop, 40);
+            console.log("gameLoop() called!")
+            preloaded = true;
+            setInterval(gameLoop, gameLoopInterval);
         } else {
             // 200ms timeout
             console.log("waiting for " + preloadCount + " asset(s) to load!")
@@ -89,18 +155,120 @@ function preloadAssets() {
     checkResources();
 }
 
+// draw assets
+function drawAssets() {
+
+    drawBackground();
+
+    //drawObstacles();
+    if (!settingsOpened) {
+        drawBird();
+    }
+
+    if (!gamePlaying || !settingsOpened) {
+        drawMainScreen();
+    }
+
+}
+
 // draw background
 function drawBackground() {
 
-    // draw world background
-    ctx.drawImage(world, 0, 0, backgroundSize[0], backgroundSize[1],
-        0, 0, backgroundSize[0], backgroundSize[1]);
+    // draw world background (left part)
+    ctx.drawImage(worldImage, background.startLeft, background.startTop, background.width, background.height,
+        -((animation * speed) % background.width), 0, background.width, background.height);
 
-    // draw bird
-    ctx.drawImage(bird, 6, 1473, birdSize[0], birdSize[1],
-        (canvas.width / 2 - (birdSize[0] / 2)), (canvas.height / 2 - (birdSize[0] / 2)), birdSize[0], birdSize[1]);
+    // draw world background (right part)
+    ctx.drawImage(worldImage, background.startLeft, background.startTop, background.width, background.height,
+        -((animation * speed) % background.width) + background.width, 0, background.width, background.height);
 
 }
+
+function drawObstacles() {
+
+    // draw obstacle
+    ctx.drawImage(obstacleImage, obstacle.topStartLeft, obstacle.topStartTop, obstacle.topWidth, obstacle.topHeight,
+        ((canvas.width / 2) - (obstacle.topWidth / 2)), -200, obstacle.topWidth, obstacle.topHeight);
+
+}
+
+function drawBird() {
+
+    // draw bird
+    ctx.drawImage(
+        birdImage,
+
+        bird.startLeft + (bird.width * birdVersion), bird.startTop + (bird.height * birdVersion),
+        bird.width, bird.height,
+
+        (canvas.width / 2 - (bird.width / 2)), (canvas.height / 2 - (bird.height / 2)),
+        bird.width, bird.height);
+
+}
+
+function drawMainScreen() {
+
+    // draw flappy bird logo
+    ctx.drawImage(
+        // image
+        flappyBirdImage,
+
+        // start pos x, start pos y
+        flappyBird.startLeft, flappyBird.startTop,
+        // width, height
+        flappyBird.width, flappyBird.height,
+
+        // canvas pos x, canvas pos y
+        ((canvas.width / 2) - (flappyBird.width / 2)), ((canvas.height / 2) - (flappyBird.height / 2) - 275),
+        // width on canvas, height on canvas
+        flappyBird.width, flappyBird.height);
+
+    // draw settings button
+    ctx.drawImage(
+        // image
+        settingsButtonImage,
+
+        // start pos x, start pos y
+        settingsButton.startLeft, settingsButton.startTop,
+        // width, height
+        settingsButton.width, settingsButton.height,
+
+        // canvas pos x, canvas pos y
+        ((canvas.width / 2) - (settingsButton.width / 2) - 100), ((canvas.height / 2) - (settingsButton.height / 2) + 275) + settingsPress,
+        // width on canvas, height on canvas
+        settingsButton.width, settingsButton.height);
+
+    // draw play button
+    ctx.drawImage(
+        // image
+        playButtonImage,
+
+        // start pos x, start pos y
+        playButton.startLeft, playButton.startTop,
+        // width, height
+        playButton.width, playButton.height,
+
+        // canvas pos x, canvas pos y
+        ((canvas.width / 2) - (playButton.width / 2) + 100), ((canvas.height / 2) - (playButton.height / 2) + 275),
+        // width on canvas, height on canvas
+        playButton.width, playButton.height);
+
+}
+
+function buttonPressed(e) {
+    if (ctx.isPointInPath(settingsButtonImage, e.offsetX, e.offsetY)) {
+        console.log("settings button pressed!")
+        settingsPress = 2;
+    }
+}
+
+function buttonReleased(e) {
+
+}
+
+
+document.getElementById("gameDiv").addEventListener("mousedown", buttonPressed);
+document.getElementById("gameDiv").addEventListener("mouseup", buttonReleased);
 
 // wait until everything is loaded
 document.addEventListener("DOMContentLoaded", init);
